04c9785d8c744b15cd2514c94f6fa453ef932a1d
Olha <olgazinevych@gmail.com>
2025-06-26 14:22:59 +0300

    feat: implement User creation endpoint

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from user.views import RegisterUserView
+
 urlpatterns = [
+    path("register/", RegisterUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
