672c46fb86e95713889cebebcffef0034a4b4097
Olha <olgazinevych@gmail.com>
2025-06-26 14:41:20 +0300

    feat: implement User list and retrieve functionality for authenticated users only

--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from user.views import RegisterUserView
+from user.views import RegisterUserView, UserViewSet
+
+router = DefaultRouter()
+router.register("", UserViewSet, basename="user")
 
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include(router.urls)),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,7 +1,17 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
