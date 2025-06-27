7363c85c507d821119673b9c89f7881f28ebd854
Olha <olgazinevych@gmail.com>
2025-06-27 09:59:23 +0300

    feat: give admin users permission to delete users and update user emails and names

--- user/serializers.py
+++ user/serializers.py
@@ -16,3 +16,18 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password")
+        user = super(UserSerializer, self).update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+
+class UserAdminUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "first_name", "last_name")
--- user/views.py
+++ user/views.py
@@ -1,17 +1,23 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, viewsets, permissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserAdminUpdateSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserViewSet(
-    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
-):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (IsAuthenticated(),)
+        return (IsAdminUser(),)
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return UserSerializer
+        return UserAdminUpdateSerializer
