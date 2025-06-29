46acc05a0be228c694f7317a1044ef9af81d2774
Olha <olgazinevych@gmail.com>
2025-06-29 20:31:10 +0300

    feat: add base Borrowing model and register it in admin

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ["expected_return_date"]
+
+    def __str__(self):
+        return f"{self.book.title} due {self.expected_return_date}"
