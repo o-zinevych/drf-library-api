d62c4273f7a63fbaffceb577e34f37cec2bc099d
Olha <olgazinevych@gmail.com>
2025-06-29 13:04:27 +0300

    feat: implement base BookSerializer and ViewSet with default router for urls

---
+++ books/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from books.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Book.objects.all(),
+                fields=["title", "author"],
+                message="This book already exists.",
+            )
+        ]
