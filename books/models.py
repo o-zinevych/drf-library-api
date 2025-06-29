0a2312289ca362009c5354db625933e3d52a01ad
Olha <olgazinevych@gmail.com>
2025-06-29 12:58:43 +0300

    feat: add ordering and unique constraint for title and author to Book model Meta

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.utils.translation import gettext_lazy as _
 
 
@@ -13,6 +14,14 @@ class Book(models.Model):
     cover = models.CharField(max_length=4, choices=Cover.choices)
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ["title", "author"]
+        constraints = [
+            UniqueConstraint(
+                fields=["title", "author"], name="unique_title_and_author_combination"
+            )
+        ]
 
     def __str__(self):
         return f"{self.title} by {self.author}"
