9147422e02ff0b6f9141a9b2221f9a44782d0e0d
Olha <olgazinevych@gmail.com>
2025-07-01 14:01:50 +0300

    feat: add Borrowing dates validation inside the model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from books.models import Book
 
@@ -16,3 +19,31 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"{self.book.title} due {self.expected_return_date}"
+
+    @staticmethod
+    def validate_future_date(date, borrowing_date_attr, error_to_raise):
+        """Validates expected return date."""
+        if date <= datetime.date.today():
+            raise error_to_raise(
+                {borrowing_date_attr: f"This date must be in the future."}
+            )
+
+    @staticmethod
+    def validate_today_or_past_date(date, borrowing_date_attr, error_to_raise):
+        """Validates borrow and actual return dates."""
+        if date > datetime.date.today():
+            raise error_to_raise(
+                {borrowing_date_attr: f"This date must be today or in the past."}
+            )
+
+    def clean(self):
+        self.validate_today_or_past_date(
+            self.borrow_date, "borrow_date", ValidationError
+        )
+
+        self.validate_future_date(
+            self.expected_return_date, "expected_return_date", ValidationError
+        )
+        self.validate_today_or_past_date(
+            self.actual_return_date, "actual_return_date", ValidationError
+        )
