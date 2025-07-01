a2f92ae279861fb1ae11f963b77ff0334d632804
Olha <olgazinevych@gmail.com>
2025-07-01 14:21:36 +0300

    feat: add Borrowing detail endpoint with its own serializer and full book detail

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 
+from books.serializers import BookSerializer
 from borrowings.models import Borrowing
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
     book = serializers.SlugRelatedField(read_only=True, slug_field="title")
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user_id",
+        )
+
+
+class BorrowingDetailSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
 
     class Meta:
         model = Borrowing
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets, mixins
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingListSerializer
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
 
 
-class BorrowingViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class BorrowingViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
     queryset = Borrowing.objects.select_related("book", "user")
-    serializer_class = BorrowingListSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
