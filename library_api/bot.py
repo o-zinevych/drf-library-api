f62526ea1d1f7bcc4531923a0bc5af6b855044f8
Olha <olgazinevych@gmail.com>
2025-07-04 10:15:55 +0300

    feat: make start command begin a conversation by asking for the user's email

--- library_api/bot.py
+++ library_api/bot.py
@@ -3,7 +3,12 @@ import os
 
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    ConversationHandler,
+)
 
 load_dotenv()
 
@@ -11,18 +16,23 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
+EMAIL = 1
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Welcome to our library notification bot. Track your borrowings with ease!",
+    await update.message.reply_text(
+        text="Welcome to our library notification bot. Track your borrowings with ease!"
+        "\nPlease, enter your email to start.",
     )
+    return EMAIL
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(os.getenv("TG_BOT_TOKEN")).build()
 
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+    )
+    application.add_handler(conv_handler)
 
     application.run_polling()
