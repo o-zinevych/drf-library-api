1b8a8086d8acba04f8fd1eefc1a0f9d94790ef3e
Olha <olgazinevych@gmail.com>
2025-07-09 15:01:53 +0300

    feat: implement drf_spectacular with a Swagger UI endpoint

--- library_api/settings.py
+++ library_api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "drf_spectacular",
     "rest_framework",
     "user",
     "books",
@@ -151,6 +152,14 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 15,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library API",
+    "DESCRIPTION": "Allows users to manage accounts, books, borrowings and receive telegram notifications.",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
 
 # Simple JWT settings
